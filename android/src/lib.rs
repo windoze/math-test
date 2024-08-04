020233167631470a9497430d1e9a00f9efd216e7
Chen Xu <windoze@0d0a.com>
2024-08-05 00:19:32 +0800

    Data persistent

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -1,5 +1,22 @@
+use app_dirs::{get_app_root, AppDataType, AppInfo};
+use log::error;
+
+const APP_INFO: AppInfo = AppInfo {
+    name: "MathQuiz",
+    author: "ChenXu",
+};
+
 #[no_mangle]
 fn android_main(app: slint::android::AndroidApp) {
     slint::android::init(app).unwrap();
-    gui_lib::ui_main(None).unwrap();
+
+    let db_path = match get_app_root(AppDataType::UserData, &APP_INFO) {
+        Ok(p) => Some(p.parent().unwrap().join("math-quiz.db")),
+        Err(e) => {
+            error!("Failed to get app data directory: {}", e);
+            None
+        }
+    };
+
+    gui_lib::ui_main(db_path).unwrap();
 }
