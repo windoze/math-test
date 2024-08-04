e2ab3f2e2dceaf71d1c5a353b2c32513dddd37b3
Chen Xu <windoze@0d0a.com>
2024-08-04 23:56:18 +0800

    WIP, cannot access storage

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -1,5 +1,5 @@
 #[no_mangle]
 fn android_main(app: slint::android::AndroidApp) {
     slint::android::init(app).unwrap();
-    gui_lib::ui_main(Option::<&std::path::Path>::None).unwrap();
+    gui_lib::ui_main(None).unwrap();
 }
